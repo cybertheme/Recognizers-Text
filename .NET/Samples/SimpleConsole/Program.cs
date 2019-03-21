c76e65d58e247923664d922f70ee17f3f3a32fa7
Kyryll <kyrvlasiuk@dev.cyberthe.me>
2019-03-21 17:09:24 +0200

    Slava Ukraini

--- .NET/Samples/SimpleConsole/Program.cs
+++ .NET/Samples/SimpleConsole/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Recognizers.Text;
-using Microsoft.Recognizers.Text.Choice;
-using Microsoft.Recognizers.Text.DateTime;
+//using Microsoft.Recognizers.Text.Choice;
+//using Microsoft.Recognizers.Text.DateTime;
 using Microsoft.Recognizers.Text.Number;
-using Microsoft.Recognizers.Text.NumberWithUnit;
-using Microsoft.Recognizers.Text.Sequence;
+//using Microsoft.Recognizers.Text.NumberWithUnit;
+//using Microsoft.Recognizers.Text.Sequence;
 using Newtonsoft.Json;
 
 namespace SimpleConsole
@@ -14,7 +14,7 @@ namespace SimpleConsole
     public class Program
     {
         // Use English for the Recognizers culture
-        private const string DefaultCulture = Culture.English;
+        private const string DefaultCulture = Culture.Ukrainian;
 
         public static void Main(string[] args)
         {
@@ -72,55 +72,55 @@ namespace SimpleConsole
 
                 // Age recognizer will find any age number presented
                 // E.g "After ninety five years of age, perspectives change" will return "95 Year"
-                NumberWithUnitRecognizer.RecognizeAge(query, culture),
+                //NumberWithUnitRecognizer.RecognizeAge(query, culture),
 
-                // Currency recognizer will find any currency presented
-                // E.g "Interest expense in the 1988 third quarter was $ 75.3 million" will return "75300000 Dollar"
-                NumberWithUnitRecognizer.RecognizeCurrency(query, culture),
+                //// Currency recognizer will find any currency presented
+                //// E.g "Interest expense in the 1988 third quarter was $ 75.3 million" will return "75300000 Dollar"
+                //NumberWithUnitRecognizer.RecognizeCurrency(query, culture),
 
-                // Dimension recognizer will find any dimension presented
-                // E.g "The six-mile trip to my airport hotel that had taken 20 minutes earlier in the day took more than three hours." will return "6 Mile"
-                NumberWithUnitRecognizer.RecognizeDimension(query, culture),
+                //// Dimension recognizer will find any dimension presented
+                //// E.g "The six-mile trip to my airport hotel that had taken 20 minutes earlier in the day took more than three hours." will return "6 Mile"
+                //NumberWithUnitRecognizer.RecognizeDimension(query, culture),
 
-                // Temperature recognizer will find any temperature presented
-                // E.g "Set the temperature to 30 degrees celsius" will return "30 C"
-                NumberWithUnitRecognizer.RecognizeTemperature(query, culture),
+                //// Temperature recognizer will find any temperature presented
+                //// E.g "Set the temperature to 30 degrees celsius" will return "30 C"
+                //NumberWithUnitRecognizer.RecognizeTemperature(query, culture),
 
-                // Datetime recognizer This model will find any Date even if its write in colloquial language
-                // E.g "I'll go back 8pm today" will return "2017-10-04 20:00:00"
-                DateTimeRecognizer.RecognizeDateTime(query, culture),
+                //// Datetime recognizer This model will find any Date even if its write in colloquial language
+                //// E.g "I'll go back 8pm today" will return "2017-10-04 20:00:00"
+                //DateTimeRecognizer.RecognizeDateTime(query, culture),
 
-                // PhoneNumber recognizer will find any phone number presented
-                // E.g "My phone number is ( 19 ) 38294427."
-                SequenceRecognizer.RecognizePhoneNumber(query, culture),
+                //// PhoneNumber recognizer will find any phone number presented
+                //// E.g "My phone number is ( 19 ) 38294427."
+                //SequenceRecognizer.RecognizePhoneNumber(query, culture),
 
-                // Add IP recognizer - This recognizer will find any Ipv4/Ipv6 presented
-                // E.g "My Ip is 8.8.8.8"
-                SequenceRecognizer.RecognizeIpAddress(query, culture),
+                //// Add IP recognizer - This recognizer will find any Ipv4/Ipv6 presented
+                //// E.g "My Ip is 8.8.8.8"
+                //SequenceRecognizer.RecognizeIpAddress(query, culture),
 
-                // Mention recognizer will find all the mention usages
-                // E.g "@Cicero"
-                SequenceRecognizer.RecognizeMention(query, culture),
+                //// Mention recognizer will find all the mention usages
+                //// E.g "@Cicero"
+                //SequenceRecognizer.RecognizeMention(query, culture),
 
-                // Hashtag recognizer will find all the hash tag usages
-                // E.g "task #123"
-                SequenceRecognizer.RecognizeHashtag(query, culture),
+                //// Hashtag recognizer will find all the hash tag usages
+                //// E.g "task #123"
+                //SequenceRecognizer.RecognizeHashtag(query, culture),
 
-                // Email recognizer will find all the emails
-                // E.g "a@b.com"
-                SequenceRecognizer.RecognizeEmail(query, culture),
+                //// Email recognizer will find all the emails
+                //// E.g "a@b.com"
+                //SequenceRecognizer.RecognizeEmail(query, culture),
 
-                // URL recognizer will find all the urls
-                // E.g "bing.com"
-                SequenceRecognizer.RecognizeURL(query, culture),
+                //// URL recognizer will find all the urls
+                //// E.g "bing.com"
+                //SequenceRecognizer.RecognizeURL(query, culture),
 
-                // GUID recognizer will find all the GUID usages
-                // E.g "{123e4567-e89b-12d3-a456-426655440000}"
-                SequenceRecognizer.RecognizeGUID(query, culture),
+                //// GUID recognizer will find all the GUID usages
+                //// E.g "{123e4567-e89b-12d3-a456-426655440000}"
+                //SequenceRecognizer.RecognizeGUID(query, culture),
 
-                // Add Boolean recognizer - This model will find yes/no like responses, including emoji -
-                // E.g "yup, I need that" will return "True"
-                ChoiceRecognizer.RecognizeBoolean(query, culture),
+                //// Add Boolean recognizer - This model will find yes/no like responses, including emoji -
+                //// E.g "yup, I need that" will return "True"
+                //ChoiceRecognizer.RecognizeBoolean(query, culture),
                 });
         }
 
