d8cadf08e4012247408430b4fb672b6692cf3dd0
Kirill Savchenko <k.savchenko@devrain.com>
2019-03-26 17:04:38 +0200

    Added spec files + Ukrainian Tests

---
+++ .NET/Microsoft.Recognizers.Text.DataDrivenTests/Number/TestNumber_Ukrainian.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Recognizers.Text.DataDrivenTests.Number
+{
+    [TestClass]
+    class TestNumber_Ukrainian : TestBase
+    {
+        public static TestResources TestResources { get; protected set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            TestResources = new TestResources();
+            TestResources.InitFromTestContext(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestSpecInitialize(TestResources);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "NumberModel-Ukrainian.csv", "NumberModel-Ukrainian#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void NumberModel()
+        {
+            TestNumber();
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "OrdinalModel-Ukrainian.csv", "OrdinalModel-Ukrainian#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void OrdinalModel()
+        {
+            TestNumber();
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "PercentModel-Ukrainian.csv", "PercentModel-Ukrainian#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void PercentModel()
+        {
+            TestNumber();
+        }
+    }
+}
